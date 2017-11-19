224b4c3d4a2a4571211ca4d34d915671cf609d8e
collin <collin.inbox@gmail.com>
2017-11-18 21:48:47 -0500

    got all the non angualr functions but still commenting them

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,26 @@
+var util = require('../lib/util.js'),
+    msg = require('../lib/msg.js');
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Nodemon rocks');
+Parse.Cloud.define('icontains', function (req, res) {
+    var value = msg.sanitize(req.params.value);
+    var answer = '';
+    var list = ['a', 'b', 'c'];
+    if (util.icontains(list, value)) {
+        answer = 'does contain';
+    } else {
+        answer = 'does not contain';
+    }
+    res.success('list ' + answer + ' ' + value);
+});
+
+Parse.Cloud.define('objecthaskey', function (req, res) {
+    var key = msg.sanitize(req.params.key);
+    var answer = '';
+    var obj = {"make": "porsche", "model": "cayman"};
+    if (util.objectHasKey(obj, key)) {
+        answer = 'has key';
+    } else {
+        answer = 'does not have key';
+    }
+    res.success('object ' + answer + ' ' + key);
 });
